ead03d755cced46a00b676dc9a6a17feb726f5bb
ragger.break <ragger.break@gmail.com>
2020-09-24 11:41:36 +0200

    8.4: Project Lombok Configuration

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -1,10 +1,15 @@
 package guru.sfg.brewery.domain.security;
 
-import org.springframework.security.core.GrantedAuthority;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -15,15 +20,23 @@ public class User {
     private String username;
     private String password;
 
+    @Singular
     @ManyToMany(cascade = CascadeType.MERGE)
     @JoinTable(name = "user_authority",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
 
+    @Builder.Default
     private boolean accountNonExpired = true;
+
+    @Builder.Default
     private boolean accountNonLocked = true;
+
+    @Builder.Default
     private boolean credentialsNonExpired = true;
+
+    @Builder.Default
     private boolean enabled = true;
 
 }
